55a16404d5282bd3a4c9718740eacde5c0eb3f76
Mosbycr <mosbycr@gmail.com>
2019-06-30 17:25:55 -0400

    added Navbar component to App.js, created Navbar component js

--- bookshelf/client/src/App.js
+++ bookshelf/client/src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Home from "./pages/HomeSearch";
 // import Saved from "./pages/Saved";
-// import Nav from "./components/Nav";
+import Nav from "./components/Navbar";
 
 function App() {
   return (
     // <Router>
     <div>
-      <h2>This is Showing!!!</h2>
-      {/* <Nav />
-        <Switch>
+      {/* <h2>This is Showing!!!</h2> */}
+      <Navbar />
+        {/* <Switch>
           <Route exact path="/" component={Books} />
           <Route exact path="/books" component={Books} />
           <Route exact path="/books/:id" component={Detail} />
