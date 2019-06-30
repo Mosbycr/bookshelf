b2881c97d894b35d53eb3618712c87b5809b7aca
Mosbycr <mosbycr@gmail.com>
2019-06-30 18:07:53 -0400

    added jumbotron component to saved page

--- bookshelf/client/src/pages/Saved.js
+++ bookshelf/client/src/pages/Saved.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
 
 class Saved extends Component {
   render() {
-    return <div>Hello this is your saved books!</div>;
+    return (
+      <Jumbotron>
+        <h1>Saved Books</h1>
+        <h3>Pick one and add it to your bookshelf!</h3>
+      </Jumbotron>
+    );
   }
 }
 
