72e13e9ed3fe6add7d702bc39323ca5bc56fddaf
Mosbycr <mosbycr@gmail.com>
2019-06-30 18:10:10 -0400

    kept jumbotron props set to children

--- bookshelf/client/src/components/Jumbotron/index.js
+++ bookshelf/client/src/components/Jumbotron/index.js
@@ -6,8 +6,6 @@ function Jumbotron({children}){
         <div className="jumbotron">
             <Container fluid>
                 {children}
-                {/* <h1>BookShelf</h1>
-                <h3>Find a book to add to your library</h3> */}
             </Container>
         </div>
     )
