c6edc2a97243208b0ba53dfff90d8655917d5fd3
Mosbycr <mosbycr@gmail.com>
2019-06-30 17:33:14 -0400

    added list component, added routes within router in app.js

--- bookshelf/client/src/App.js
+++ bookshelf/client/src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Home from "./pages/HomeSearch";
-// import Saved from "./pages/Saved";
-import Nav from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/HomeSearch";
+import Saved from "./pages/Saved";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    // <Router>
+    <Router>
     <div>
-      {/* <h2>This is Showing!!!</h2> */}
       <Navbar />
-        {/* <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch> */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/books" component={Home} />
+          <Route exact path="/books/:id" component={Saved} />
+          {/* <Route component={NoMatch} /> */}
+        </Switch>
     </div>
-    // </Router>
+    </Router>
   );
 }
 
