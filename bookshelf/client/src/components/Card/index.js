9078aa9f2356fcf43f4aaa736968e9d5c5a5f7d1
Mosbycr <mosbycr@gmail.com>
2019-07-02 15:06:40 -0400

    added in props for card

--- bookshelf/client/src/components/Card/index.js
+++ bookshelf/client/src/components/Card/index.js
@@ -5,8 +5,12 @@ export function Card(props){
       <div className="card">
         <img src={props.img} className="card-img-top" alt="" />
         <div className="card-body">
-          <h5 className="card-title" />
-          <p className="card-text" />
+          <h3 className="card-title">Title: {props.title}</h3>
+          <h2 className="card-author">Author(s): {props.authors}</h2>
+          <p className="card-text">Synopsis: {props.description}</p>
+          <a href={props.link} className="btn btn-primary">
+            Buy Book
+          </a>
           <a href="#" className="btn btn-primary">
             Save Book
           </a>
