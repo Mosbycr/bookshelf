6193cb1dda037a82028beb4aeb8af6d0ebe1e317
Mosbycr <mosbycr@gmail.com>
2019-07-05 12:51:34 -0400

    changed card into class

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React, {Component} from "react";
+// import API from "../utils/API";
 
-export function Card(props){
+
+class Card extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      savedbooks: [],
+    };
+
+  }
+
+  render(){
     return (
       <div className="card">
-        <img src={props.img} className="card-img-top" alt="" />
+        <img src={this.props.img} className="card-img-top" alt="" />
         <div className="card-body">
-          <h3 className="card-title">Title: {props.title}</h3>
-          <h2 className="card-author">Author(s): {props.authors}</h2>
-          <p className="card-text">Synopsis: {props.description}</p>
-          <a href={props.link} className="btn btn-primary">
+          <h3 className="card-title">Title: {this.props.title}</h3>
+          <h2 className="card-author">Author(s): {this.props.authors}</h2>
+          <p className="card-text">Synopsis: {this.props.description}</p>
+          <a href={this.props.link} className="btn btn-primary">
             Buy Book
           </a>
-          {
+          <button className="btn btn-primary">Save Book</button>
+          {/* {
             (this.props.path === "/" || this.props.path === "/search")? 
           <button className="btn btn-primary">
             Save Book
           </button> : null
-          }
+          } */}
         </div>
       </div>
     );
+  }
 }
+
+
+export default Card
